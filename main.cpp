16066dfe89e5de8d0c318c0b33cc51ecd24d1699
Michael1297 <38764610+Michael1297@users.noreply.github.com>
2022-02-16 17:01:23 +0300

    upd

--- main.cpp
+++ main.cpp
@@ -3,10 +3,8 @@
 #include "nlohmann/json.hpp"
 #include "gumbo.h"
 
+
 int main() {
     cpr::Response r = cpr::Get(cpr::Url("http://httpbin.org"));
-    auto* parse = gumbo_parse(r.text.c_str());
-
-    std::cout << r.text;
-
+    
 }
